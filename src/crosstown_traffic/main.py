6dddc0179a590e20db641b4dd546dba7cf29efd4
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-06-01 18:37:11 -0400

    fix: test

--- src/crosstown_traffic/main.py
+++ src/crosstown_traffic/main.py
@@ -42,7 +42,7 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
-    return {"message": "Nothing here, bababababa!"}
+    return {"message": "Nothing here, babaasdasdadasdbababa!"}
 
 @app.post("/api/mta")
 async def mta(request: TrafficRequest):
